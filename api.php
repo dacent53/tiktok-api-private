5ed30e0370fcc3bff244cf318c038bb4dfe0711b
dacent53 <gneo0194@gmail.com>
2026-01-12 19:38:36 +0300

    Update api.php

--- api.php
+++ api.php
@@ -1,4 +1,4 @@
 ### contact me
 
-**Contact for api :** [https://t.me/alethino](https://t.me/alethino)
-## telegram: @alethino
+**Contact for api :** [https://t.me/dacent53](https://t.me/dacent53)
+## telegram: @dacent53
